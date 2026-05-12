3a95079a255dfda094b367260675df43ba10c7ad
rangaNP <ranganirmal789@gmail.com>
2026-05-12 08:51:47 +0530

    fix:admin login issue

--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginCard } from "@/components/auth/login-card";
-
-export default function LoginPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-surface p-4">
-      <LoginCard />
-    </div>
-  );
-}
